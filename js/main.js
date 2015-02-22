c478c429b754b1a1e424ac23e21bcc3d57d2af7f
Chris Mueller <cmuellerrr@gmail.com>
2015-02-22 13:52:01 -0800

    added back autosave feture

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
             onplaying: function (event) {
                 playButton.addClass('fa-pause').removeClass('fa-play');
             },
-                    
+
             onpause: function (event) {
                 playButton.addClass('fa-play').removeClass('fa-pause');
             },
@@ -20,7 +20,10 @@ $(document).ready(function () {
                 playTime.text(formatSeconds(this.currentTime));
                 $('#progress').width(((this.currentTime / this.duration) * 100) + "%");
             }
-        });
+        }),
+        storage;
+
+    loadFromLocalStorage();
 
     //toggle play/pause if you click on the play/pause button
     playButton.on("click", function (event) {
@@ -47,7 +50,7 @@ $(document).ready(function () {
 
         if (url) {
             controller.loadAudio(url);
-            $('.tTitle').focus();
+            $('#title').focus();
         }
         else {
             alert("Error loading audio file");
@@ -130,6 +133,12 @@ $(document).ready(function () {
         }
     });
 
+    //add logic to auto save to the browser cache after 2 seconds idle
+    $('.saved').typing({
+        stop: saveToLocalStorage,
+        delay: 500
+    });
+
     /* Utilitiy Functions */
 
     //Provide feedback that a command was executed
@@ -142,7 +151,7 @@ $(document).ready(function () {
     };
 
     //Add a timestamp, including newlines, at the cursor position.
-    //Handle special cases where enter is pressed while in the middle 
+    //Handle special cases where enter is pressed while in the middle
     //of a sentence or if you've selevted a range.
     //TODO inefficient to get the node each time
     var addTimestampAtCursor = function (time) {
@@ -174,22 +183,61 @@ $(document).ready(function () {
         resizeBody();
     };
 
+    //Load the transcript from the browser's cache if it exists
+    function loadFromLocalStorage() {
+        //load from storage
+        if ('localStorage' in window && window['localStorage'] !== null) {
+            storage = window.localStorage;
+
+            if (storage.getItem('ts_title') ||
+                storage.getItem('ts_subtitle') ||
+                storage.getItem('ts_text')) {
+
+                console.log("RESTORING FROM CACHE");
+
+                $('#title').val(storage['ts_title'] );
+                $('#subtitle').val(storage['ts_subtitle']);
+                $('#text').val(storage['ts_text']);
+
+                resizeBody();
+            }
+        }
+    }
+
+    //Save the transcript to the browser's cache
+    //
+    //TODO this currently relies on loadFromLocalStorage being called
+    //first to set up the storage var
+    function saveToLocalStorage() {
+        if (storage && $("#cmd-save").prop('checked')) {
+            console.log("SAVING");
+
+            $('#save-status').text("Saving...");
+
+            storage['ts_title'] = $('#title').val();
+            storage['ts_subtitle'] = $('#subtitle').val();
+            storage['ts_text'] = $('#text').val();
+
+            setTimeout(function() {
+                $('#save-status').text("Last saved @ " + new Date().toLocaleTimeString());
+            }, 750);
+        }
+    }
+
     //Resize the body textarea based off of the content within it.
     //Copy over the text to a hidden div so we can get its size
     //then set the text area to that size.
-    var resizeBody = (function() {
+    function resizeBody() {
         var body = $('#text'),
             hiddenBody = $('#hiddenText');
 
-        return function () {
-            hiddenBody.html(body.val().replace(/\n/g, '<br>'));
+        hiddenBody.html(body.val().replace(/\n/g, '<br>'));
 
-            if (body.height() < hiddenBody.height()) {
-                body.css('height', hiddenBody.height());
-                window.scrollBy(0, 50);
-            }
-        };
-    })();
+        if (body.height() < hiddenBody.height()) {
+            body.css('height', hiddenBody.height());
+            window.scrollBy(0, 50);
+        }
+    }
 
     //Format the given number as a timestamp [hh:mm:ss]
     var formatSecondsAsTimestamp = function (secs) {
